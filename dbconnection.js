d22f67d726f4af93502a6c7bd726c7ddfee2a832
tharindu <tnnmuhandiram@gmail.com>
2019-05-15 10:35:57 +0530

    db connection change

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,17 @@
+// var mysql = require('mysql');
+// var connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '1234',
+//   database: 'nodesql'
+// });
+// module.exports = connection;
+
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
+    host: '35.197.180.104',
+    user: 'rem138c7',
+    password: 'nodesql@2019',
   database: 'nodesql'
 });
 module.exports = connection;
